76c928eebfa63d54281c33ab75ca6f5b106c6bb7
Jaden Rieck <jar629@drexel.edu>
2025-11-19 08:50:13 -0500

    added 11ty configuration to output css to website

---
+++ eleventy.config.js
@@ -0,0 +1,8 @@
+export default async function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('css')
+	return {
+		passthroughFileCopy: true
+	}
+	// this tells eleventy to look for folder named css and copy it through to output folder
+	// aka _site
+};
