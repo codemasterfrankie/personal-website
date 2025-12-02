ec43b6849ee7fd5e28ecac6c1a95c71b3f03d065
Jaden Rieck <jar629@drexel.edu>
2025-12-02 10:40:55 -0500

    undid config changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function(eleventyConfig) {
+	eleventyConfig.setBrowserSyncConfig('../../css')
 	eleventyConfig.addPassthroughCopy('app.js')
-	eleventyConfig.setBrowserSyncConfig({
-		files: './_site/css/**/*.css'
-	});
+	return {
+		passthroughFileCopy: true
+	}
 	// this tells eleventy to look for folder named css and copy it through to output folder
 	// aka _site
 };
